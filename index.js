fe491e4366de66e1b0b5b45c406e7f27ef8d1337
blurrys <blurryworks@gmail.com>
2022-12-01 19:51:08 +0200

    VER.1

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+class Webhook {
+  #webhook;
+  #username;
+  constructor() {
+    this.#webhook = "";
+    this.#username = "Default";
+  }
+  login(webhook) {
+    this.#webhook = webhook;
+  }
+  #checkWebhook() {
+    let pattern = /(https?)?(:\/\/)?(www.)?discord.com\/api\/webhooks/gi;
+    return pattern.test(this.#webhook);
+  }
+  setUsername(username) {
+    if (typeof username !== "string") {
+      throw "Username can be only a string!";
+    } else {
+      this.#username = username;
+    }
+  }
+  send(message) {
+    if (this.#checkWebhook() === true) {
+      axios.post(this.#webhook, {
+        username: this.#username,
+        content: message,
+      });
+    } else {
+      throw "Invalid URL";
+    }
+  }
+}
